f044e1d2468f00a4aaef8bf4c48380773559c202
admin <admin@corp.netease.com>
2020-01-03 18:49:47 +0800

    [SVN-SYNC] 删除中文路径文件 Revision:20 Date: 2019-11-25 06:24:38.452347Z

--- Unity_Learning/网易作业/TpsProject/Assets/Scripts/Tps_Weapon.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Tps_Weapon : MonoBehaviour
-{
-    public string WeaponName;
-}
--- Unity_Learning/网易作业/服务器部分/Server/DBCreate.py
+++
@@ -1,9 +0,0 @@
-# -*- coding:utf-8 -*-
-import shelve
-
-s=shelve.open("user1.db")
-
-try:
-    s["vivid"]={'password':"8888", 'bulletCount': 60, 'chargerBulletCount':5000, 'playerHealth': 100, 'weaponInfo':"武器: 1: Tps_Weapon_fal"}
-finally:
-    s.close()
